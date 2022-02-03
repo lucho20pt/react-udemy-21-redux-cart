6ffd9e33ad384bc0205df04ee052974314b84a0a
Daniel Batista <dani_batista20@hotmail.com>
2022-02-03 19:26:53 +0000

    upd: cartSlice - removeItemFromCart()

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -28,7 +28,18 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + existingItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
