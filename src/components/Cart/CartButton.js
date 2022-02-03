b98c873675a8a0fc85b57bb1801eae369fe7ba3d
Daniel Batista <dani_batista20@hotmail.com>
2022-02-03 17:27:18 +0000

    upd: CartButton - useSelector() to get totalQuantity

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,19 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { uiActions } from "store/ui-slice";
 import classes from "./CartButton.module.scss";
 
 const CartButton = (props) => {
+  //
   const dispatch = useDispatch();
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggleCart());
   };
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQuantity}</span>
     </button>
   );
 };
