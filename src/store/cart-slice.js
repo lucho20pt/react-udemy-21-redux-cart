b7d8b70a5c4ed7d2bfa0e85e641984112464d3c8
Daniel Batista <dani_batista20@hotmail.com>
2022-02-03 17:16:49 +0000

    upd: cartSlice - addItemToCart()

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,18 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialCartState = {
-    items: [],
-    totalQuantity: 0,
-    totalAmount: 0,
-    totalPrice: 0
+  items: [],
+  totalQuantity: 0,
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: initialCartState,
   reducers: {
-    addToCart() {},
-    removeFromCart() {},
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          name: newItem.title,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + existingItem.price;
+      }
+    },
+    removeItemFromCart() {},
   },
 });
 
