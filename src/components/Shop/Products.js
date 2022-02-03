a23502c11b6c12408b4f664eee852b3f05c3b7da
Daniel Batista <dani_batista20@hotmail.com>
2022-02-03 17:18:09 +0000

    upd: products - add fake data & render products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.scss';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.scss";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 10,
+    title: "First Title",
+    price: 5,
+    description: "This is a first product description",
+  },
+  {
+    id: 11,
+    title: "Second Title",
+    price: 8,
+    description: "This is a second product description",
+  },
+];
 
 const Products = (props) => {
+  //
+  const products = DUMMY_PRODUCTS;
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {products.map((product) => {
+          const { id, title, price, description } = product;
+          return (
+            <ProductItem
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+            />
+          );
+        })}
       </ul>
     </section>
   );
