f2c180fec06118830ff49d7f0e7482ba2daa5da7
Daniel Batista <dani_batista20@hotmail.com>
2022-02-03 17:20:32 +0000

    upd: productItem - useDispatch(), addItemToCartHandler()

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,16 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from 'store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.scss';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { title, price, description, id } = props;
+  
+  const dispatch = useDispatch();
+
+  const addItemToCartHandler = () => {
+    dispatch(cartActions.addItemToCart({title, price, id}))  
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +21,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addItemToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
