5c670b4437a32d0c40043cb72240b1c9804c253e
Daniel Batista <dani_batista20@hotmail.com>
2022-02-03 17:25:57 +0000

    upd: Cart - useSelector() to get cartItems & render CartItem

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,34 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.scss';
-import CartItem from './CartItem';
+import { useSelector } from "react-redux";
+import Card from "../UI/Card";
+
+import classes from "./Cart.module.scss";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart.items);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
+        {cartItems.map((item) => {
+          const { id, name, price, quantity, totalPrice } = item;
+          return (
+            <CartItem
+              key={id}
+              item={{
+                id: id,
+                title: name,
+                quantity: quantity,
+                total: totalPrice,
+                price: price,
+              }}
+            />
+          );
+        })}
+        {/* <CartItem
           item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        /> */}
       </ul>
     </Card>
   );
