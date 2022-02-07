bc504ddb9a5686b3729751598060de61906aec02
Daniel Batista <dani_batista20@hotmail.com>
2022-02-07 10:17:32 +0000

    add: useEffect() with Redux & fetchCart()

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,22 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const cartBaseUrl =
+    "https://react-udemy21-http-default-rtdb.europe-west1.firebasedatabase.app/cart.json";
+
+  useEffect(() => {
+    const fetchCart = async () => {
+      const response = await fetch(cartBaseUrl, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+      const data = await response.data;
+      console.log(data);
+    };
+    fetchCart();
+    return () => console.log("callback");
+  }, [cart]);
 
   return (
     <Layout>
