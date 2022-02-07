e006727794e53c2802b6141b4acb969c8e2067df
Daniel Batista <dani_batista20@hotmail.com>
2022-02-07 11:25:57 +0000

    add: app - dispatch uiActions.showNotification({})

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "store/ui-slice";
 
 function App() {
-  const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector((state) => state.cart);
+  //
   const cartBaseUrl =
     "https://react-udemy21-http-default-rtdb.europe-west1.firebasedatabase.app/cart.json";
+  //
+  const dispatch = useDispatch();
+  const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
+  // useEffect()
   useEffect(() => {
-    const fetchCart = async () => {
+    // sendCartData ()
+    const sendCartData = async () => {
+      //
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending data!",
+        })
+      );
+
+      // fetch
       const response = await fetch(cartBaseUrl, {
         method: "PUT",
         body: JSON.stringify(cart),
       });
-      const data = await response.data;
-      console.log(data);
+
+      if (!response.ok) {
+        throw new Error("Sending data cart failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data Successfully!",
+        })
+      );
     };
-    fetchCart();
+
+    // execute() & catch()
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data Failed!",
+        })
+      );
+    });
+
     return () => console.log("callback");
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
     <Layout>
